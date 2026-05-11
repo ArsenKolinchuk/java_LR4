be0f550aad7a8c894474bdb23d287ef04220e144
ArsenKolinchuk <162623014+ArsenKolinchuk@users.noreply.github.com>
2026-05-11 23:32:19 +0300

    Update Trainer.java

--- src/Trainer.java
+++ src/Trainer.java
@@ -5,13 +5,7 @@ public class Trainer {
     private String phoneNumber;
     private String email;
 
-    public Trainer(
-            String fullName,
-            String specialization,
-            String phoneNumber,
-            String email
-    ) {
-
+    public Trainer(String fullName, String specialization, String phoneNumber, String email) {
         this.fullName = fullName;
         this.specialization = specialization;
         this.phoneNumber = phoneNumber;
