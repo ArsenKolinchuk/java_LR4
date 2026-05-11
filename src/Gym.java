2cf2dc7b8218bf6d599dd16d6dd60c258f41c997
ArsenKolinchuk <162623014+ArsenKolinchuk@users.noreply.github.com>
2026-05-11 23:30:31 +0300

    Update Gym.java

--- src/Gym.java
+++ src/Gym.java
@@ -8,33 +8,21 @@ public class Gym {
         this.name = name;
         this.address = address;
     }
-
     public void showGym() {
-
         System.out.println("Gym: " + name);
         System.out.println("Address: " + address);
     }
-
+    
     // static nested class
     static class Subscription {
-
         private String type;
         private int days;
-
         public Subscription(String type, int days) {
-
             this.type = type;
             this.days = days;
         }
-
         void showSubscription() {
-
-            System.out.println(
-                    "Subscription: "
-                            + type
-                            + ", Days: "
-                            + days
-            );
+            System.out.println("Subscription: "+ type+ ", Days: "+ days);
         }
     }
 
@@ -45,22 +33,13 @@ public class Gym {
         private String phoneNumber;
         private String email;
 
-        Administrator(
-                String fullName,
-                String phoneNumber,
-                String email
-        ) {
-
+        Administrator(String fullName, String phoneNumber, String email) {
             this.fullName = fullName;
             this.phoneNumber = phoneNumber;
             this.email = email;
         }
-
         void manageGym() {
-
-            System.out.println(
-                    fullName + " manages the gym."
-            );
+            System.out.println(fullName + " manages the gym.");
         }
     }
 }
